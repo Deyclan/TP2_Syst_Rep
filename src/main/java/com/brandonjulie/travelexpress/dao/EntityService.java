83d1ad0bfbe5768f889e3c8366818a9c8ae846c3
brandon.piard@etu.univ-lyon1.fr <brandon.piard@etu.univ-lyon1.fr>
2018-10-30 15:02:46 -0400

    run application configuration

--- src/main/java/com/brandonjulie/travelexpress/dao/EntityService.java
+++ src/main/java/com/brandonjulie/travelexpress/dao/EntityService.java
@@ -1,4 +1,20 @@
 package com.brandonjulie.travelexpress.dao;
 
-public class EntityService {
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public abstract class EntityService {
+
+    protected EntityManager entityManager;
+    protected EntityManagerFactory emf;
+
+    public EntityTransaction startTransaction() throws Exception {
+        emf = Persistence.createEntityManagerFactory("travel_express");
+        entityManager = emf.createEntityManager();
+
+        return entityManager.getTransaction();
+    }
+
 }
